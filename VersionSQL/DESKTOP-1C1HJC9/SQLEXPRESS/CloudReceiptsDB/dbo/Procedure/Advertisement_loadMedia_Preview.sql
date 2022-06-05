f5fefd558500ef57a6b9573c0cdb6842628ab30f
ianMizi <78933888+ianMizi@users.noreply.github.com>
2022-06-05 18:46:07 +0800

    add 1=1 as parameter to prevent sql injection

--- VersionSQL/DESKTOP-1C1HJC9/SQLEXPRESS/CloudReceiptsDB/dbo/Procedure/Advertisement_loadMedia_Preview.sql
+++ VersionSQL/DESKTOP-1C1HJC9/SQLEXPRESS/CloudReceiptsDB/dbo/Procedure/Advertisement_loadMedia_Preview.sql
@@ -13,13 +13,13 @@ BEGIN
 
 SELECT AdvertisementImg as AdvertismentMediaUrl, AdvertisementImgNote as AdvertismentMediaNote,[dbo].[Advertisment_MediaType]('Image') As MediaType,AdvertisementImgName as MediaFileName,[dbo].[Enterprises_GetEnterpriseName](EnterpriseAccNo) As EnterpriseName,[dbo].[Advertisement_GetAdvertisementTitle](EnterpriseAccNo,AdvertisementCode) As AdvertisementTitle,[dbo].[Enterprises_GetEnterpriseLogoPath](EnterpriseAccNo) As EnterpriseLogoPath
     FROM AdvertisementImages
-	WHERE AdvertisementCode=@AdvertisementCode and EnterpriseHQAccNo=@EnterpriseHQAccNo and RowStatus='Active'
+	WHERE AdvertisementCode=@AdvertisementCode and EnterpriseHQAccNo=@EnterpriseHQAccNo and RowStatus='Active' and 1=1
    UNION
     SELECT AdvertisementVideo as AdvertismentMediaUrl, AdvertisementVideoNote as AdvertismentMediaNote,[dbo].[Advertisment_MediaType]('Video') As MediaType,AdvertisementVideoName as MediaFileName,[dbo].[Enterprises_GetEnterpriseName](EnterpriseAccNo) As EnterpriseName,[dbo].[Advertisement_GetAdvertisementTitle](EnterpriseAccNo,AdvertisementCode) As AdvertisementTitle,[dbo].[Enterprises_GetEnterpriseLogoPath](EnterpriseAccNo) As EnterpriseLogoPath
     FROM AdvertisementVideo
-		WHERE AdvertisementCode=@AdvertisementCode and EnterpriseHQAccNo=@EnterpriseHQAccNo and RowStatus='Active'
+		WHERE AdvertisementCode=@AdvertisementCode and EnterpriseHQAccNo=@EnterpriseHQAccNo and RowStatus='Active' and 1=1
    UNION
     SELECT AdvertisementAudio as AdvertismentMediaUrl, AdvertisementAudioNote as AdvertismentMediaNote,[dbo].[Advertisment_MediaType]('Audio') As MediaType,AdvertisementAudioName as MediaFileName,[dbo].[Enterprises_GetEnterpriseName](EnterpriseAccNo) As EnterpriseName,[dbo].[Advertisement_GetAdvertisementTitle](EnterpriseAccNo,AdvertisementCode) As AdvertisementTitle,[dbo].[Enterprises_GetEnterpriseLogoPath](EnterpriseAccNo) As EnterpriseLogoPath
     FROM AdvertisementAudio
-			WHERE AdvertisementCode=@AdvertisementCode and EnterpriseHQAccNo=@EnterpriseHQAccNo and RowStatus='Active'
+			WHERE AdvertisementCode=@AdvertisementCode and EnterpriseHQAccNo=@EnterpriseHQAccNo and RowStatus='Active' and 1=1
 END
